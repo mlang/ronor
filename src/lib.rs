b0e07b12595c471407811113b9d14f847fa1e1c6
Mario Lang <mlang@blind.guru>
2019-09-22 20:58:41 +0200

    refresh

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use oauth2::basic::BasicClient;
-use oauth2::{AccessToken, AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl};
+use oauth2::{AccessToken, AuthorizationCode, AuthUrl, ClientId, ClientSecret, RedirectUrl, RefreshToken, TokenResponse, TokenUrl};
+use oauth2::reqwest::http_client;
 use reqwest::{Client};
 use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::fs::{read_to_string, write};
 use serde::{Deserialize, Serialize};
 use url::Url;
 
@@ -14,8 +17,23 @@ error_chain!{
       description("PlayerId is unknown")
       display("Failed to find PlayerId")
     }
+    IntegrationRequired {
+      description("integration required for this operation")
+      display("No integration configuration found")
+    }
+    TokenRequired {
+      description("access_token required for this operation")
+      display("No access_token available")
+    }
+    TokenExpired {
+      description("access_token is likely expired")
+      display("Failed to call with status code 401")
+    }
   }
   foreign_links {
+    IO(std::io::Error);
+    TOMLDeserialization(toml::de::Error);
+    TOMLSerialization(toml::ser::Error);
     UrlParse(url::ParseError);
     SerdeJson(serde_json::error::Error);
     Request(reqwest::Error);
@@ -23,12 +41,12 @@ error_chain!{
 }
 
 pub fn oauth2(
-  client_id: ClientId, client_secret: ClientSecret, redirect_url: RedirectUrl
+  client_id: &ClientId, client_secret: &ClientSecret, redirect_url: &RedirectUrl
 ) -> Result<BasicClient> {
-  Ok(BasicClient::new(client_id, Some(client_secret),
+  Ok(BasicClient::new(client_id.clone(), Some(client_secret.clone()),
       AuthUrl::new(Url::parse("https://api.sonos.com/login/v3/oauth")?),
       Some(TokenUrl::new(Url::parse("https://api.sonos.com/login/v3/oauth/access")?))
-    ).set_redirect_url(redirect_url)
+    ).set_redirect_url(redirect_url.clone())
   )
 }
 
@@ -109,40 +127,9 @@ struct Households {
   households: Vec<Household>
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Household {
   id: HouseholdId
-}
-
-impl Household {
-  pub fn get_groups(self: &Self, tok: &AccessToken) -> Result<Groups> {
-    let client = Client::new();
-    Ok(
-      client
-      .get(&format!("https://api.ws.sonos.com/control/api/v1/households/{}/groups",
-                    self.id))
-      .bearer_auth(tok.secret()).send()?.error_for_status()?.json()?
-    )
-  }
-  pub fn get_favorites(self: &Self, tok: &AccessToken) -> Result<Favorites> {
-    let client = Client::new();
-    Ok(
-      client
-      .get(&format!("https://api.ws.sonos.com/control/api/v1/households/{}/favorites",
-                    self.id))
-      .bearer_auth(tok.secret()).send()?.error_for_status()?.json()?
-    )
-  }
-}
-
-pub fn get_households(
-  tok: &AccessToken
-) -> Result<Vec<Household>> {
-  let client = Client::new();
-  let households: Households = client
-    .get("https://api.ws.sonos.com/control/api/v1/households")
-    .bearer_auth(tok.secret()).send()?.error_for_status()?.json()?;
-  Ok(households.households)
 }
 
 #[derive(Debug, Deserialize)]
@@ -161,52 +148,6 @@ pub struct Group {
   pub name: String
 }
 
-impl Group {
-  pub fn get_playback_status(&self, tok: &AccessToken) -> Result<PlaybackStatus> {
-    let client = Client::new();
-    Ok(
-      client
-      .get(&format!("https://api.ws.sonos.com/control/api/v1/groups/{}/playback",
-                    self.id))
-      .bearer_auth(tok.secret()).send()?.error_for_status()?.json()?
-    )
-  }
-  pub fn get_volume(self: &Self, tok: &AccessToken) -> Result<GroupVolume> {
-    let client = Client::new();
-    Ok(
-      client
-      .get(&format!("https://api.ws.sonos.com/control/api/v1/groups/{}/groupVolume",
-                    self.id))
-      .bearer_auth(tok.secret()).send()?.error_for_status()?.json()?
-    )
-  }
-  pub fn set_volume(self: &Self, tok: &AccessToken, volume: u8) -> Result<()> {
-    let client = Client::new();
-    let mut params = HashMap::new();
-    params.insert("volume", volume);
-    client
-      .put(&format!("https://api.ws.sonos.com/control/api/v1/groups/{}/groupVolume",
-                    self.id))
-      .bearer_auth(tok.secret()).send()?.error_for_status()?;
-    Ok(())
-  }
-  pub fn load_favorite(self: &Self,
-    tok: &AccessToken, favorite_id: &FavoriteId, play_on_completion: bool
-  ) -> Result<()> {
-    let params = LoadFavorite {
-      favorite_id: favorite_id.clone(),
-      play_on_completion: play_on_completion,
-      play_modes: None
-    };
-    let client = Client::new();
-    client
-      .post(&format!("https://api.ws.sonos.com/control/api/v1/groups/{}/favorites",
-                     self.id))
-      .bearer_auth(tok.secret()).json(&params).send()?.error_for_status()?;
-    Ok(())
-  }
-}
-
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Player {
@@ -217,55 +158,6 @@ pub struct Player {
   pub name: String,
   pub software_version: String,
   pub capabilities: Vec<Capability>
-}
-
-impl Player {
-  pub fn get_volume(
-    self: &Self, tok: &AccessToken
-  ) -> Result<PlayerVolume> {
-    let client = Client::new();
-    Ok(
-      client
-      .get(&format!("https://api.ws.sonos.com/control/api/v1/players/{}/playerVolume",
-                    self.id))
-      .bearer_auth(tok.secret()).send()?.error_for_status()?.json()?
-    )
-  }
-  pub fn load_audio_clip(
-    self: &Self, tok: &AccessToken,
-    app_id: String, name: String,
-    clip_type: Option<AudioClipType>, priority: Option<Priority>, volume: Option<u8>,
-    http_authorization: Option<String>, stream_url: Option<String>
-  ) -> Result<AudioClip> {
-    let client = Client::new();
-    let mut params = HashMap::new();
-    params.insert("appId", app_id);
-    params.insert("name", name);
-    if let Some(clip_type) = clip_type {
-      params.insert("clipType", serde_json::to_string(&clip_type)?);
-    }
-    if let Some(priority) = priority {
-      params.insert("priority", serde_json::to_string(&priority)?);
-    }
-    if let Some(volume) = volume {
-      params.insert("volume", volume.to_string());
-    }
-    if let Some(stream_url) = stream_url {
-      params.insert("streamUrl", stream_url);
-    }
-    if let Some(http_authorization) = http_authorization {
-      params.insert("httpAuthorization", http_authorization);
-    }
-    let mut audio_clip: AudioClip = client
-      .post(&format!("https://api.ws.sonos.com/control/api/v1/players/{}/audioClip",
-                     self.id))
-      .bearer_auth(tok.secret())
-      .json(&params)
-      .send()?.error_for_status()?
-      .json()?;
-    audio_clip.player_id = Some(self.id.clone());
-    Ok(audio_clip)
-  }
 }
 
 #[derive(Debug, Deserialize)]
@@ -324,7 +216,7 @@ pub struct PlaybackStatus {
   available_playback_actions: AvailablePlaybackActions,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PlayModes {
   repeat: bool,
@@ -354,7 +246,7 @@ pub struct Favorites {
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Favorite {
-  id: String,
+  id: FavoriteId,
   name: String,
   description: Option<String>,
   image_url: Option<String>,
@@ -384,4 +276,305 @@ pub struct IntegrationConfig {
   pub client_secret: ClientSecret,
   pub redirect_url: RedirectUrl
 }
+#[derive(Clone, Deserialize, Serialize)]
+struct Tokens {
+  access_token: AccessToken,
+  refresh_token: RefreshToken
+}
 
+pub struct Sonos {
+  integration: Option<IntegrationConfig>,
+  integration_path: Option<std::path::PathBuf>,
+  tokens: Option<Tokens>,
+  tokens_path: Option<std::path::PathBuf>
+}
+
+impl Sonos {
+  pub fn is_registered(self: &Self) -> bool { self.integration.is_some() }
+
+  pub fn is_authorized(self: &Self) -> bool { self.tokens.is_some() }
+
+  pub fn authorization_url(self: &Self
+  ) -> Result<(url::Url, oauth2::CsrfToken)> {
+    match &self.integration {
+      Some(integration) => {
+        let auth = oauth2(&integration.client_id, &integration.client_secret,
+          &integration.redirect_url
+        )?;
+        Ok(auth.authorize_url(oauth2::CsrfToken::new_random).add_scope(oauth2::Scope::new("playback-control-all".to_string())).url())
+      },
+      None => Err(ErrorKind::IntegrationRequired.into())
+    }
+  }
+
+  pub fn authorize(self: &mut Self,
+    code: AuthorizationCode
+  ) -> Result<()> {
+    match &self.integration {
+      Some(integration) => {
+        let auth = oauth2(&integration.client_id, &integration.client_secret,
+          &integration.redirect_url
+        )?;
+        let token_result = auth.exchange_code(code).request(http_client).unwrap();
+        if let Some(refresh_token) = token_result.refresh_token() {
+          self.tokens = Some(Tokens { access_token: token_result.access_token().clone()
+                              , refresh_token: refresh_token.clone()
+                               });
+          let toml = toml::to_string_pretty(&self.tokens.as_ref().unwrap())?;
+          if let Some(path) = &self.tokens_path {
+            write(path, toml)?;
+          }
+          Ok(())
+        } else {
+          Err("No refresh token received".into())
+        }
+      },
+      None => Err(ErrorKind::IntegrationRequired.into())
+    }
+  }
+  
+  pub fn refresh_token(self: &mut Self) -> Result<&mut Self> {
+    match &self.integration {
+      Some(integration) => {
+        let auth = oauth2(&integration.client_id, &integration.client_secret,
+          &integration.redirect_url
+        )?;
+        match &self.tokens {
+          Some(tokens) => {
+            let token_response = auth.exchange_refresh_token(&tokens.refresh_token)
+              .request(http_client).unwrap();
+            self.tokens = Some(Tokens {
+                access_token: token_response.access_token().clone(),
+                refresh_token: token_response.refresh_token().unwrap_or(&tokens.refresh_token).clone()
+              }
+            );
+            if let Some(tokens_path) = &self.tokens_path {
+              write(tokens_path, toml::to_string_pretty(self.tokens.as_ref().unwrap())?)?;
+            }
+            Ok(self)
+          },
+          None => Err(ErrorKind::TokenRequired.into())
+        }
+      },
+      None => Err(ErrorKind::IntegrationRequired.into())
+    }
+  }
+
+  fn maybe_refresh<T>(
+    self: &mut Self,
+    call: &dyn Fn(&AccessToken) -> Result<reqwest::Response>,
+    convert: &dyn Fn(reqwest::Response) -> Result<T>
+  ) -> Result<T> {
+    match &self.tokens {
+      Some(tokens) => convert({
+        let response = call(&tokens.access_token)?;
+        if response.status() == reqwest::StatusCode::UNAUTHORIZED {
+          self.refresh_token()?;
+          call(&self.tokens.as_ref().unwrap().access_token)?
+        } else {
+          response
+        }
+      }.error_for_status()?),
+      None => Err(ErrorKind::TokenRequired.into())
+    }
+  }
+
+  pub fn get_households(self: &mut Self
+  ) -> Result<Vec<Household>> {
+    self.maybe_refresh(&|access_token| {
+      let client = Client::new();
+      Ok(
+        client
+          .get("https://api.ws.sonos.com/control/api/v1/households")
+          .bearer_auth(access_token.secret())
+          .send()?
+      )
+    }, &|mut response| {
+      let households: Households = response.json()?;
+      Ok(households.households)
+    })
+  }
+  pub fn get_groups(self: &mut Self,
+    household: &Household
+  ) -> Result<Groups> {
+    self.maybe_refresh(&|access_token| {
+      let client = Client::new();
+      Ok(
+        client
+          .get(&format!("https://api.ws.sonos.com/control/api/v1/households/{}/groups", household.id))
+          .bearer_auth(access_token.secret()).send()?
+      )
+    }, &|mut response| Ok(response.json()?)
+    )
+  }
+  pub fn get_favorites(self: &mut Self,
+    household: &Household
+  ) -> Result<Favorites> {
+    self.maybe_refresh(&|access_token| {
+      let client = Client::new();
+      Ok(
+        client
+          .get(&format!("https://api.ws.sonos.com/control/api/v1/households/{}/favorites",
+                        household.id))
+          .bearer_auth(access_token.secret()).send()?
+      )
+    }, &|mut response| Ok(response.json()?)
+    )
+  }
+  pub fn get_playback_status(self: &mut Self,
+    group: &Group
+  ) -> Result<PlaybackStatus> {
+    self.maybe_refresh(&|access_token| {
+      let client = Client::new();
+      Ok(
+        client
+          .get(&format!("https://api.ws.sonos.com/control/api/v1/groups/{}/playback",
+                        group.id))
+          .bearer_auth(access_token.secret()).send()?
+      )
+    }, &|mut response| Ok(response.json()?)
+    )
+  }
+  pub fn load_favorite(self: &mut Self,
+    group: &Group,
+    favorite: &Favorite,
+    play_on_completion: bool,
+    play_modes: &Option<PlayModes>
+  ) -> Result<()> {
+    self.maybe_refresh(&|access_token| {
+      let params = LoadFavorite {
+        favorite_id: favorite.id.clone(),
+        play_on_completion: play_on_completion,
+        play_modes: play_modes.clone()
+      };
+      let client = Client::new();
+      Ok(
+        client
+          .post(&format!("https://api.ws.sonos.com/control/api/v1/groups/{}/favorites", group.id))
+          .bearer_auth(access_token.secret()).json(&params).send()?
+      )
+    }, &|_response| Ok(())
+    )
+  }
+  pub fn get_group_volume(self: &mut Self,
+    group: &Group
+  ) -> Result<GroupVolume> {
+    self.maybe_refresh(&|access_token| {
+      let client = Client::new();
+      Ok(
+        client
+          .get(&format!("https://api.ws.sonos.com/control/api/v1/groups/{}/groupVolume", group.id))
+          .bearer_auth(access_token.secret()).send()?
+      )
+    }, &|mut response| Ok(response.json()?)
+    )
+  }
+  pub fn set_group_volume(self: &mut Self,
+    group: &Group,
+    volume: u8
+  ) -> Result<()> {
+    self.maybe_refresh(&|access_token| {
+      let client = Client::new();
+      let mut params = HashMap::new();
+      params.insert("volume", volume);
+      Ok(
+        client
+          .put(&format!("https://api.ws.sonos.com/control/api/v1/groups/{}/groupVolume", group.id))
+          .bearer_auth(access_token.secret()).send()?
+      )
+    }, &|_response| Ok(())
+    )
+  }
+
+  pub fn get_player_volume(self: &mut Self,
+    player: &Player
+  ) -> Result<PlayerVolume> {
+    self.maybe_refresh(&|access_token| {
+      let client = Client::new();
+      Ok(
+        client
+          .get(&format!("https://api.ws.sonos.com/control/api/v1/players/{}/playerVolume",
+                        player.id))
+          .bearer_auth(access_token.secret()).send()?
+      )
+    }, &|mut response| Ok(response.json()?)
+    )
+  }
+  pub fn load_audio_clip(self: &mut Self,
+    player: &Player, app_id: String, name: String,
+    clip_type: Option<AudioClipType>, priority: Option<Priority>, volume: Option<u8>,
+    http_authorization: Option<String>, stream_url: Option<String>
+  ) -> Result<AudioClip> {
+    self.maybe_refresh(&|access_token| {
+      let client = Client::new();
+      let mut params = HashMap::new();
+      params.insert("appId", app_id.clone());
+      params.insert("name", name.clone());
+      if let Some(clip_type) = &clip_type {
+        params.insert("clipType", serde_json::to_string(&clip_type)?);
+      }
+      if let Some(priority) = &priority {
+        params.insert("priority", serde_json::to_string(&priority)?);
+      }
+      if let Some(volume) = volume {
+        params.insert("volume", volume.to_string());
+      }
+      if let Some(stream_url) = &stream_url {
+        params.insert("streamUrl", stream_url.clone());
+      }
+      if let Some(http_authorization) = &http_authorization {
+        params.insert("httpAuthorization", http_authorization.clone());
+      }
+      Ok(
+        client
+          .post(&format!("https://api.ws.sonos.com/control/api/v1/players/{}/audioClip",
+                         player.id))
+          .bearer_auth(access_token.secret())
+          .json(&params)
+          .send()?
+      )
+    }, &|mut response| {
+      let mut audio_clip: AudioClip = response.json()?;
+      audio_clip.player_id = Some(player.id.clone());
+      Ok(audio_clip)
+    })
+  }
+}
+
+impl TryFrom<xdg::BaseDirectories> for Sonos {
+  type Error = Error;
+  fn try_from(xdg_dirs: xdg::BaseDirectories) -> Result<Self> {
+    let integration_config_path = xdg_dirs.place_config_file("sonos_integration.toml")?;
+    let tokens_config_path = xdg_dirs.place_config_file("sonos_tokens.toml")?;
+    match read_to_string(&integration_config_path).and_then(|s| Ok(toml::from_str(&s)?)) {
+      Ok(integration) => match read_to_string(&tokens_config_path).and_then(|s| Ok(toml::from_str(&s)?)) {
+        Ok(tokens) => Ok(Sonos {
+          integration: Some(integration),
+          integration_path: Some(integration_config_path),
+          tokens: Some(tokens),
+          tokens_path: Some(tokens_config_path)
+        }),
+        Err(_) => Ok(Sonos {
+          integration: Some(integration),
+          integration_path: Some(integration_config_path),
+          tokens: None,
+          tokens_path: Some(tokens_config_path)
+        })
+      },
+      Err(_) => match read_to_string(&tokens_config_path).and_then(|s| Ok(toml::from_str(&s)?)) {
+        Ok(tokens) => Ok(Sonos {
+          integration: None,
+          integration_path: Some(integration_config_path),
+          tokens: Some(tokens),
+          tokens_path: Some(tokens_config_path)
+        }),
+        Err(_) => Ok(Sonos {
+          integration: None,
+          integration_path: Some(integration_config_path),
+          tokens: None,
+          tokens_path: Some(tokens_config_path)
+        })
+      }
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,99 @@
-use oauth2::{
-  AccessToken,
-  AuthorizationCode,
-  CsrfToken,
-  RefreshToken,
-  Scope,
-  TokenResponse,
-};
-use oauth2::reqwest::http_client;
-use serde::{Deserialize, Serialize};
-use std::fs::{File};
-use std::io::{stdin, Read};
-use std::process::Command;
-use ronor::*;
+use clap::{Arg, App, SubCommand};
+use oauth2::AuthorizationCode;
+use ronor::{IntegrationConfig, Sonos};
+use rustyline::Editor;
+use std::process::{Command, Stdio};
+use std::convert::TryFrom;
+use xdg::BaseDirectories;
 
-#[derive(Deserialize, Serialize)]
-struct Tokens {
-  access_token: AccessToken,
-  refresh_token: RefreshToken
+#[macro_use]
+extern crate error_chain;
+
+error_chain! {
+  links {
+    API(ronor::Error, ronor::ErrorKind);
+  }
+  foreign_links {
+    IO(std::io::Error);
+    XDG(xdg::BaseDirectoriesError);
+    ReadLine(rustyline::error::ReadlineError);
+  }
 }
 
 fn main() -> Result<()> {
-  let xdg_dirs = xdg::BaseDirectories::with_prefix("ronor").unwrap();
-  let integration_config_path = xdg_dirs.place_config_file("integration.toml").expect("Cannot create configuration directory");
-  let tokens_config_path = xdg_dirs.place_config_file("tokens.toml").expect("cannot create configuration directory");
+  let matches = App::new("ronor")
+    .about("Does awesome things")
+    .subcommand(SubCommand::with_name("login")
+      .about("Refresh the access token")
+    ).subcommand(SubCommand::with_name("refresh")
+      .about("Refresh the access token")
+    ).subcommand(SubCommand::with_name("speak")
+      .about("Refresh the access token")
+    ).subcommand(SubCommand::with_name("getHouseholds")
+      .about("Get households (DEBUG)")
+    )
+    .get_matches();
+  let mut sonos = Sonos::try_from(BaseDirectories::with_prefix("ronor")?)?;
+  if let Some(matches) = matches.subcommand_matches("login") {
+    let (auth_url, csrf_token) = sonos.authorization_url()?;
+    let _lynx = Command::new("lynx")
+      .arg("-nopause")
+      .arg(auth_url.as_str())
+      .status().expect("Failed to fire up browser.");
+    println!("Token: {}", csrf_token.secret());
+    let mut console = Editor::<()>::new();
+    let code = console.readline("Code: ")?;
+    sonos.authorize(AuthorizationCode::new(code.trim().to_string()))?;
+    Ok(())
+  } else if let Some(matches) = matches.subcommand_matches("refresh") {
+    if !sonos.is_registered() {
+      println!("ronor is not registered, run authroize");
+    } else if !sonos.is_authorized() {
+      println!("Not authroized, can not refresh");
+    } else {
+      sonos.refresh_token()?;
+    }
+    Ok(())
+  } else if let Some(matches) = matches.subcommand_matches("speak") {
+    let espeak = Command::new("espeak")
+    .args(&[ "-a", "150"
+        , "-s", "250"
+        , "-v", "de"
+        , "-w", "/dev/stdout", "--stdin"])
+    .stdout(Stdio::piped()).spawn()?;
+    if let Some(stdout) = espeak.stdout {
+      let ffmpeg = Command::new("ffmpeg")
+        .args(&["-i", "-", "-v", "fatal", "-b:a", "96k", "-f", "mp3", "-"])
+        .stdin(stdout).stdout(Stdio::piped()).spawn()?;
+      if let Some(stdout) = ffmpeg.stdout {
+        let curl = Command::new("curl")
+          .args(&["--upload-file", "-", "https://transfer.sh/espeak.mp3"])
+          .stdin(stdout).output()?;
+        if curl.status.success() {
+          let url = String::from_utf8_lossy(&curl.stdout);
 
-  let access_token = match File::open(&tokens_config_path) {
-    Ok(mut file) => {
-      let mut tok = String::new();
-      file.read_to_string(&mut tok).expect("Error reading token from file");
-      let tokens: Tokens = toml::from_str(&tok).expect("Failed to parse toml");
-      tokens.access_token.clone()
-    },
-    Err(_) => {
-      let str = std::fs::read_to_string(integration_config_path)
-        .expect("Failed to read integration configuration");
-      let integration_config: IntegrationConfig = toml::from_str(&str).expect("Failed to parse integration configuration");
-      let auth = oauth2(
-        integration_config.client_id,
-        integration_config.client_secret,
-        integration_config.redirect_url
-      )?;
-      let (auth_url, csrf_token) = auth
-        .authorize_url(CsrfToken::new_random)
-        .add_scope(Scope::new("playback-control-all".to_string()))
-        .url();
-      let _lynx = Command::new("lynx")
-        .arg("-nopause")
-        .arg(auth_url.as_str())
-        .status().expect("Failed to fire up browser.");
-      println!("Token: {}", csrf_token.secret());
-      let mut code = String::new();
-      stdin().read_line(&mut code).expect("unable to read user input");
-      let code = AuthorizationCode::new(code.to_string().trim().to_string());
-      let token_result = auth.exchange_code(code).request(http_client)
-        .expect("Token result error");
-      if let Some(refresh_token) = token_result.refresh_token() {
-        let tokens = Tokens { access_token: token_result.access_token().clone()
-                            , refresh_token: refresh_token.clone()
-                            };
-        let toml = toml::to_string_pretty(&tokens).unwrap();
-        std::fs::write(&tokens_config_path, toml).expect("Failed to write tokens");
+          let households = sonos.get_households()?;
+          for household in households {
+            let groups = sonos.get_groups(&household)?;
+            for player in groups.players.iter().filter(|p| p.name == "Wohnzimmer") {
+              let _clip = sonos.load_audio_clip(&player,
+                String::from("guru.blind"), String::from("ping"), None, None, None,
+                None, Some(url.to_string())
+              )?;
+            }
+          }
+        }
       }
-      token_result.access_token().clone()
     }
-  };
-
-  let households = get_households(&access_token)?;
-  if households.len() == 1 {
-    println!("{:?}", households[0].get_favorites(&access_token)?);
-    let groups = households[0].get_groups(&access_token)?;
-    println!("{:?}", groups);
-    for player in groups.players {
-      let clip = player.load_audio_clip(&access_token,
-        String::from("guru.blind"), String::from("ping"), None, None, None, None, None
-      )?;
-      println!("{:?}", clip);
-      println!("{:?}", player.get_volume(&access_token));
+    Ok(())
+  } else if let Some(matches) = matches.subcommand_matches("getHouseholds") {
+    if !sonos.is_authorized() {
+      println!("Not authroized, can not refresh");
+    } else {
+      println!("{:?}", sonos.get_households());
     }
-    for group in groups.groups {
-      let group_volume = group.get_volume(&access_token)?;
-      println!("{:?}", group_volume);
-      println!("{:?}", group.get_playback_status(&access_token)?);
-    }
+    Ok(())
+  } else {
+    Ok(())
   }
-
-  Ok(())
 }
