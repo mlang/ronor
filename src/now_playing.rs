a8da330af1522b092ce02300d206430fc3d584b4
Mario Lang <mlang@blind.guru>
2019-10-15 12:39:27 +0200

    report HT

--- src/now_playing.rs
+++ src/now_playing.rs
@@ -1,7 +1,6 @@
 use clap::{Arg, ArgMatches, App};
 use ronor::{Sonos, PlaybackState};
-use std::process::exit;
-use super::Result;
+use super::{Result, ErrorKind};
 
 pub const NAME: &str = "now-playing";
 
@@ -12,21 +11,26 @@ pub fn build() -> App<'static, 'static> {
 }
 
 pub fn run(sonos: &mut Sonos, matches: &ArgMatches) -> Result<()> {
+  let group_name = matches.value_of("GROUP");
   let mut found = false;
   for household in sonos.get_households()?.iter() {
     for group in sonos.get_groups(&household)?.groups.iter().filter(|group|
-      matches.value_of("GROUP").map_or(true, |name| name == group.name)
+      group_name.map_or(true, |name| name == group.name)
     ) {
       found = true;
       if group.playback_state == PlaybackState::Playing {
         let metadata_status = sonos.get_metadata_status(&group)?;
         let mut parts = Vec::new();
         if let Some(container) = &metadata_status.container {
-          if let Some(name) = &container.name {
-            parts.push(name.as_str());
-          }
-          if let Some(service) = &container.service {
-            parts.push(service.name.as_str());
+          if container.type_.is_some() && container.type_.as_ref().unwrap() == "linein.homeTheater" {
+            parts.push("Home theater");
+          } else {
+            if let Some(name) = &container.name {
+              parts.push(name.as_str());
+            }
+            if let Some(service) = &container.service {
+              parts.push(service.name.as_str());
+            }
           }
         }
         if let Some(current_item) = &metadata_status.current_item {
@@ -55,9 +59,11 @@ pub fn run(sonos: &mut Sonos, matches: &ArgMatches) -> Result<()> {
       }
     }
   }
-  if matches.value_of("GROUP").is_some() && !found {
-    println!("The specified group was not found");
-    exit(1);
+  if !found {
+    if let Some(group_name) = group_name {
+      return Err(ErrorKind::UnknownGroup(group_name.to_string()).into());
+    }
+    return Err("No groups found".into());
   }
   Ok(())
 }
