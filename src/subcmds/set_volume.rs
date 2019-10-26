306ed9bb8d2d00260d83e896c4feb268a4a886fb
Mario Lang <mlang@blind.guru>
2019-10-27 00:08:03 +0200

    -i and -d flags to avoid negative relative volume to be interpreted as arg

--- src/subcmds/set_volume.rs
+++ src/subcmds/set_volume.rs
@@ -9,10 +9,20 @@ pub fn build() -> App<'static, 'static> {
     .about("Set volume for a group or player")
     .arg(crate::household_arg())
     .arg(
-      Arg::with_name("RELATIVE")
-        .short("r")
-        .long("relative")
-        .help("Indicates that the volume should be interpreted as relative")
+      Arg::with_name("INCREMENT")
+        .short("i")
+        .long("increment")
+        .help("Increase volume")
+    )
+    .arg(
+      Arg::with_name("DECREMENT")
+        .short("d")
+        .long("decrement")
+        .help("Decrease volume")
+    )
+    .group(
+      ArgGroup::with_name("RELATIVE")
+        .args(&["INCREMENT", "DECREMENT"])
     )
     .arg(
       Arg::with_name("GROUP")
@@ -43,19 +53,24 @@ pub fn build() -> App<'static, 'static> {
 pub fn run(sonos: &mut Sonos, matches: &ArgMatches) -> Result<()> {
   let household = matches.household(sonos)?;
   let targets = sonos.get_groups(&household)?;
-  let relative = matches.is_present("RELATIVE");
+  let increment = matches.is_present("INCREMENT");
+  let decrement = matches.is_present("DECREMENT");
   let volume = matches.value_of("VOLUME").unwrap();
   if matches.is_present("GROUP") {
     let group = matches.group(&targets.groups)?;
-    if relative {
+    if increment {
       sonos.set_relative_group_volume(&group, volume.parse()?)
+    } else if decrement {
+      sonos.set_relative_group_volume(&group, -volume.parse()?)
     } else {
       sonos.set_group_volume(&group, volume.parse()?)
     }
   } else {
     let player = matches.player(&targets.players)?;
-    if relative {
+    if increment {
       sonos.set_relative_player_volume(&player, volume.parse()?)
+    } else if decrement {
+      sonos.set_relative_player_volume(&player, -volume.parse()?)
     } else {
       sonos.set_player_volume(&player, volume.parse()?)
     }
